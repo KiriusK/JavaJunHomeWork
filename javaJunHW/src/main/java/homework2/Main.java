95935e029cd465d8c070107c6905ea4e7dde65e1
KiriusK <kvarton@rambler.ru>
2024-11-26 19:54:39 +0300

    Second homework

---
+++ javaJunHW/src/main/java/homework2/Main.java
@@ -0,0 +1,24 @@
+package homework2;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class<?> clazz = Class.forName("java.lang.String");
+        Method[] methods = clazz.getMethods();
+        int count = 1;
+        for (Method meth:methods) {
+            System.out.printf("Метод №%d: %s\n", count, meth);
+            count++;
+        }
+        String s = "Hello";
+        Class<?> strCls = s.getClass();
+        Field[] fields = strCls.getFields();
+        count = 1;
+        for (Field fld:fields) {
+            System.out.printf("Поле №%d: %s\n", count, fld);
+            count++;
+        }
+    }
+}
